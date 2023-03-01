90099cbaae9f5b04a7c2316d81d43cbc3b2d5463
Stephen <pena731.junk@gmail.com>
2023-03-01 08:45:48 -0500

    first-class-function

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution in this file!
+
+const  Drivers = ["Antonia", "Nuru", "Amari", "Mo"];  
+
+// function 1
+const returnFirstTwoDrivers = function (a){
+return a.slice(0,a.length - 2);
+}
+returnFirstTwoDrivers(Drivers);
+
+// function 2
+const returnLastTwoDrivers = function (b){
+    return b.slice(-2);
+    }
+returnLastTwoDrivers(Drivers);
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+//calculate function
+const fareQuintupler = (cfm)=>{
+    let var1 = (cfm * 5);
+        console.log( var1);
+        return var1;
+}
+
+
+// function 3
+function createFareMultiplier(cf){
+   
+   
+   const fareMultiplier = function (fm){
+    let var2 = (fm * cf);
+    //console.log( var2);
+    return var2;
+}
+           return fareMultiplier;       
+    }
+createFareMultiplier(5);
+
+
+//function 4
+const fareDoubler = createFareMultiplier(2);
+
+//function 5.
+const fareTripler = createFareMultiplier(3);
+
+
+function selectDifferentDrivers(drvpara,firstdr){
+
+return firstdr(drvpara);
+
+}
+selectDifferentDrivers(Drivers,returnFirstTwoDrivers);
+selectDifferentDrivers(Drivers,returnLastTwoDrivers);
